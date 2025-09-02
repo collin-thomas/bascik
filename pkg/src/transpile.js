df9eefb627c485e9c5c8afb4cae0a19b76c102eb
collin-thomas <collin.inbox@gmail.com>
2025-09-02 14:16:20 -0700

    Fix transpile imports

--- pkg/src/transpile.js
+++ pkg/src/transpile.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 import { createRequire } from "module";
 import { createSelfSignedCert } from "./lib/pki.js";
-const require = createRequire(import.meta.url);
-const { BascikConfig } =  require("./lib/config.js");
-const { watchFiles } = require("./lib/watch.js");
+import { BascikConfig } from "./lib/config.js";
+import { watchFiles } from "./lib/watch.js";
 
 watchFiles();
 
 if (!BascikConfig.isBuild) {
   createSelfSignedCert()
+  const require = createRequire(import.meta.url);
   const { serveHttp2 } = require("./lib/http2.js");
   serveHttp2();
 }
